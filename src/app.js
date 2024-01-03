30f057e2c20a2355ce70361ae4e19f731cc44048
nelnelnelnel <ophx823@gmail.com>
2024-01-03 04:52:55 -0500

    yubtby

--- src/app.js
+++ src/app.js
@@ -38,6 +38,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // ...
 const checkForToken = (req, res, next) => {
